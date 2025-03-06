5635790e17f5ecd9627fd2b41615e13cf715ce37
Alenta-Nixon <alentanixon84@gmail.com>
2025-03-06 15:26:29 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,25 @@
+console.log("Hello \nworld")
+console.log("Good")
+console.log("Morning")
+console.log("Hello")
+console.log("Heyy")
+console.log("oii")
+console.log("Beii")
+console.log("Deyy")
+console.log("uhhh")
+console.log("kooii")
+let a=10
+const b=2
+console.log(a+b)
+console.log(a*b)
+console.log(a/b)
+console.log(a%b)
+console.log(a-b)
+
+
+
+
+
+
+
+
